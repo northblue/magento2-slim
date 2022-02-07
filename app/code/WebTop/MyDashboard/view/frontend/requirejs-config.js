732c2558ceb20fd6e4bc888410ca49b122da4711
Bin Li <northblue@gmail.com>
2022-02-07 22:34:41 +1100

    MyDashboard module js update

--- app/code/WebTop/MyDashboard/view/frontend/requirejs-config.js
+++ app/code/WebTop/MyDashboard/view/frontend/requirejs-config.js
@@ -2,7 +2,9 @@ var config = {
     map: {
         "*": {
             echarts: "WebTop_MyDashboard/js/echarts.min"
-        }
+        },
 
+// For test purpose only
+       urlArgs: "bust=" + (new Date()).getTime()
     }
 };
